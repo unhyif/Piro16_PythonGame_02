a310c93bc18a6d5053619e82ce1418be467175e9
Se Youn Lee <lsy20140@gmail.com>
2022-01-07 01:47:23 +0900

    game1

--- game1.py
+++ game1.py
@@ -1,2 +1,41 @@
-def br_game():
-    print('br_game~~')
+from main import User, Game
+import random
+import time
+
+
+menu=['떡볶이','비빔밥','김치찌개','부대찌개','치킨']
+menu_pick=random.choice(menu)
+
+def chef_game():
+    print(
+        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("                                     오늘은 내가 요리사!! 오늘은 내가 요리사!!")
+
+    print(
+        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    time.sleep(1)
+    print("오늘은 무엇을 만드나요??\n")
+    time.sleep(1)
+    print('오늘은 >>>>>',menu_pick,'<<<<< 만들어요!!!\n')
+    
+    while 1:
+        time.sleep(1)
+        ingredients=input('무슨 재료가 들어갈까요? ')
+        time.sleep(1)
+        
+        rand=random.choice([True,False])
+        if rand==True:
+            time.sleep(0.5)
+            print('있음!')
+        else:
+            time.sleep(0.5)
+            print('없음!')
+            time.sleep(1)
+            print('')
+            print('(งᐖ)ว (งᐖ)ว (งᐖ)ว (งᐖ)ว(งᐖ)ว (งᐖ)ว 빠라바라밤 빠라바라밤 빠빠바빠바밤 원샷!!!(งᐖ)ว (งᐖ)ว(งᐖ)ว (งᐖ)ว (งᐖ)ว (งᐖ)ว\n')
+            break
+
+        
+    
+    
+    
