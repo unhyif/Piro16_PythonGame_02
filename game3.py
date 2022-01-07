24eb69fd2ee9a84ab5fcedf429771417cbc3b8e9
hyoung <ycat01@naver.com>
2022-01-07 11:46:08 +0900

    [Fix] : game 3 안내 문구 수정

--- game3.py
+++ game3.py
@@ -36,16 +36,17 @@ def zero_game_run(alcohol_game):
 
             while True: #자신이 들 손가락 숫자 입력 받기
                 f_n = int(input("0, 1, 2중에서 올릴 손가락의 개수를 입력해주세요 : "))
-                if(f_n >= 0 and f_n < 4):
+                if(f_n >= 0 and f_n < 3):
                     sum += f_n #합에 더하기
                     break
                 else:
                     print("범위에 알맞은 숫자를 고르세요!")
                      
             print(g_n, '개의 손가락이 올라오면 게임이 끝납니다!')
+            print(alcohol_game.user.name, '님은 손가락', f_n, '개를 들었습니다.')
             for i in range(alcohol_game.computer_num): #컴퓨터 플레이어들이 드는 손가락 합
                 f_n = random.randint(0,2)
-                print('손가락', f_n,'개를 들었습니다.')
+                print(alcohol_game.computer_user_list[i].name,'님은 손가락', f_n,'개를 들었습니다.')
                 sum +=f_n
 
             print('총 합은 ', sum, '이였습니다!')
@@ -57,7 +58,7 @@ def zero_game_run(alcohol_game):
                 break #게임 끝
 
             else: # 다르다면 다음 턴
-                print('아쉽게도 이기지 못했습니다ㅠㅠ 다음 턴을 기대해봐요!')
+                print(alcohol_game.user.name,'님 아쉽게도 이기지 못했습니다ㅠㅠ 다음 턴을 기대해봐요!')
                 print('-------------------------')
                 alcohol_game.next_turn()
         
@@ -68,16 +69,17 @@ def zero_game_run(alcohol_game):
 
             while True: #유저 손가락 드는 게수 입력 받기
                 f_n = int(input("0, 1, 2중에서 펼칠 손가락의 개수를 입력해주세요 : "))
-                if(f_n >= 0 and f_n < 4):
+                if(f_n >= 0 and f_n < 3):
                     sum += f_n
                     break
                 else:
                     print("범위에 알맞은 숫자를 고르세요!")
 
             print(g_n, '개의 손가락이 올라오면 게임이 끝납니다!')
+            print(alcohol_game.user.name, '님은 손가락', f_n, '개를 들었습니다.')
             for i in range(alcohol_game.computer_num): #컴퓨터 유저 손가락 들기
                 f_n = random.randint(0,2)
-                print('손가락', f_n,'개를 들었습니다.')
+                print(alcohol_game.computer_user_list[i].name,'님은 손가락', f_n,'개를 들었습니다.')
                 sum +=f_n
 
             if(sum == g_n): #목표 숫자 같으면 이김
@@ -92,7 +94,7 @@ def zero_game_run(alcohol_game):
                 break #게임 끝
 
             else: #틀렸으면 다음턴
-                print('아쉽게도 이기지 못했습니다ㅠㅠ 다음 턴을 기대해봐요!')
+                print(alcohol_game.turn.name,'님 아쉽게도 이기지 못했습니다ㅠㅠ 다음 턴을 기대해봐요!')
                 print('-------------------------')
                 alcohol_game.next_turn() 
                 
